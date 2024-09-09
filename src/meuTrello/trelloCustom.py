c3c0d1141765d5aa535e8452cc6c6936c95cbd34
Laerte Jun Takeuti <laertejt@gmail.com>
2024-09-09 08:55:58 -0300

    melhorias

--- src/meuTrello/trelloCustom.py
+++ src/meuTrello/trelloCustom.py
@@ -61,22 +61,37 @@ class TrelloCustom(TrelloApi):
         df_checkitem = self.pegar_df_checkitems(checkitem_id)
         return df_checkitem
     
-    def criar_card_mensal(self, org_name, board_name, list_name, card_name, checklist_name, checkitem_name, initial_date, final_date, wday, cal) -> None:
+    def criar_card_mensal(self, df_temp1, initial_date, final_date, cal, checkitem_name=None) -> None:
+        checkitem_name = checkitem_name or "Day"
+        date_temp1 = initial_date
+        date_temp2 = final_date
+        org_name = df_temp1.organizacao.values[0]
+        board_name = df_temp1.board.values[0]
+        list_name = df_temp1.lists.values[0]
+        card_name = df_temp1.cards.values[0]
         df_list = self.consultar_df_lists(org_name, board_name)
         list_id = df_list.query("name==@list_name")['id'].values[0]
         card_name_date = f'{card_name} {initial_date.strftime("%B %Y")}'
         dic_card = super().create_card(list_id, card_name_date, initial_date, final_date)
         card_id = dic_card['id']
-        dic_checklist = super().create_checklists(card_id, checklist_name)
-        checklist_id = dic_checklist['id']
-        while initial_date <= final_date:
-            if initial_date.weekday() in wday:
-                checkitem_name_date = f'{checkitem_name} {initial_date.strftime("%d")}'
-                dic_checkitem = super().create_checkitems(checklist_id, checkitem_name_date, initial_date)
-                print(f'Card {card_name_date} criado com os CheckList {checklist_name} e CheckItem {checkitem_name_date} !!! ')
-            initial_date = cal.offset(initial_date, +1)
-            initial_date = datetime.combine(initial_date, datetime.min.time())
-            initial_date = initial_date + timedelta(hours=8,minutes=0)
-            
+        lst_checklists = list(set(df_temp1.checklists.values))
+        for checklist_name in lst_checklists:
+            df_temp2 = df_temp1.loc[df_temp1.checklists==checklist_name]
+            wday = []
+            wday += [int(i) for i in df_temp2.weekdays.values[0].split(',')]
+            dic_checklist = super().create_checklists(card_id, checklist_name)
+            checklist_id = dic_checklist['id']
+            # para zerar as datas (com as data iniciais) quando cria um novo checklist
+            initial_date = date_temp1
+            final_date = date_temp2
+            while initial_date <= final_date:
+                if initial_date.weekday() in wday:
+                    checkitem_name_date = f'{checkitem_name} {initial_date.strftime("%d")}'
+                    dic_checkitem = super().create_checkitems(checklist_id, checkitem_name_date, initial_date)
+                    print(f'Card {card_name_date} criado com os CheckList {checklist_name} e CheckItem {checkitem_name_date} !!! ')
+                initial_date = cal.offset(initial_date, +1)
+                initial_date = datetime.combine(initial_date, datetime.min.time())
+                initial_date = initial_date + timedelta(hours=8,minutes=0)
+            print("criar_card_mensal - Card Mensal criado com sucesso!!!")
 
     
